2b6a948f8457aa6459f85d18da66d79e3eecef87
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2020-05-07 21:28:22 -0700

    wifi

--- Rcm/Rcm.ino
+++ Rcm/Rcm.ino
@@ -5,22 +5,25 @@ const char *APPass = "RCMpassword";
 int port = 25210;
 const boolean connectToNetwork = true; //true=try to connect to router  false=go straight to hotspot mode
 ////////////////////////////////////////
+boolean ledVal = false;
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("##########esp32 powered on.");
   setupWifi();
+  ///////////////////
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   wifiComms();
-
+  digitalWrite(2, ledVal);
 }
 //you can communicate booleans, bytes, ints, and floats
 void WifiDataToParse() {
   wifiArrayCounter = 0;
   //add data to read here:
-  Serial.println(readIntFromBuffer());
+  ledVal = readBoolFromBuffer();
 
 }
 int WifiDataToSend() {
