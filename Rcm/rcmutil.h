b8f6686150d66a79b6745c836e44d37db6cb1e51
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2020-05-17 23:16:23 -0700

    motor stuff

--- Rcm/rcmutil.h
+++ Rcm/rcmutil.h
@@ -19,51 +19,6 @@ const int PWM_RANGE = 255;
 #define PORT_C 2,17,16,4
 #define PORT_D 3,23,22,19
 
-void enableMot(int ch, int en, int i1, int i2) {
-  ledcSetup(ch, PWM_FREQ, PWM_RES);
-  ledcAttachPin(en, ch);
-  digitalWrite(i1, HIGH);
-  digitalWrite(i2, HIGH);
-  ledcWrite(ch, 0);
-}
-void disableMot(int ch, int en, int i1, int i2) {
-  digitalWrite(i1, LOW);
-  digitalWrite(i2, LOW);
-  digitalWrite(en, LOW);
-}
-void setMot(int ch, int en, int i1, int i2, int val) {
-  val = constrain(val, -PWM_RANGE, PWM_RANGE);
-  if (val > 0) {
-    digitalWrite(i1, HIGH);
-    digitalWrite(i2, LOW);
-    ledcWrite(ch, val);
-  }  else if (val < 0) {
-    digitalWrite(i1, LOW);
-    digitalWrite(i2, HIGH);
-    ledcWrite(ch, -val);
-  }
-  else {
-    digitalWrite(i1, HIGH);
-    digitalWrite(i2, HIGH);
-    digitalWrite(en, LOW);
-  }
-}
-void setMot(int ch, int en, int i1, int i2, float val) {
-  setMot(en, i1, i2, int(val * 255);
-}
-void tankMot(int chl, int enl, int i1l, int i2l, int chr, int enr, int i1r, int i2r, int turn, int speed) {
-  setMot(enl, i1l, i2l, turn + speed);
-  setMot(enr, i1r, i2r, -turn + speed);
-}
-void tankMot(int chl, int enl, int i1l, int i2l, int chr, int enr, int i1r, int i2r, float turn, float speed) {
-  setMot(enl, i1l, i2l, int(turn * 255) + int(speed * 255));
-  setMot(enr, i1r, i2r, -int(turn * 255) + int(speed * 255));
-}
-
-void tankMot(int chl, int enl, int i1l, int i2l, int chr, int enr, int i1r, int i2r, PVector vect) {
-  setMot(enl, i1l, i2l, int(vect.x * 255) + int(vect.y * 255));
-  setMot(enr, i1r, i2r, -int(vect.x * 255) + int(vect.y * 255));
-}
 
 struct PVector {
   float x;
