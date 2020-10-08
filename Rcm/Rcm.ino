078adb4c70fe13ecd24ef499e33baca9336d4e2b
Weston Bosworth <westonbosworth@gmail.com>
2020-10-08 15:02:17 -0700

    Update Rcm.ino

--- Rcm/Rcm.ino
+++ Rcm/Rcm.ino
@@ -1,7 +1,7 @@
 #include "rcmutil.h"
 #include "wifi.h"
-const char *routerName = "networkName";
-const char *routerPass = "networkPass";
+const char *routerName = "google2";
+const char *routerPass = "5413422189";
 const char *APPass = "RCMpassword";
 int port = 25210;
 const boolean connectToNetwork = true; //true=try to connect to router  false=go straight to hotspot mode
@@ -9,23 +9,40 @@ const boolean wifiRestartNotHotspot = true; //when connection issue, true=retry 
 const int SIGNAL_LOSS_TIMEOUT = 1000; //disable if no signal after this many milliseconds
 //////////////////////////// add variables here
 
-void Enabled() { //code to run while enabled
+//PVector driveVect = {0.0, 0.0};
+float drive = 0.0;
+float turn = 0.0;
+float intake = 0.0;
+float arm = 0.0;
 
+void Enabled() { //code to run while enabled
+  setSer(port1, arm, 900, 2100);
+  setSer(port2, -arm, 900, 2100);
+  setSer(port3, intake, 950, 2100);
+  tankMot(portA, portC, turn, drive);
 }
 
 void Enable() { //turn on outputs
-
+  enableMot(portA);
+  enableMot(portC);
+  enableSer(port1);
+  enableSer(port2);
+  enableSer(port3);
 }
 
 void Disable() { //shut off all outputs
-
+  disableMot(portA);
+  disableMot(portC);
+  disableSer(port1);
+  disableSer(port2);
+  disableSer(port3);
 }
 
 void PowerOn() { //runs once on robot startup, set pinmodes
 
 }
 
-void Always(){ //always runs if void loop is running, don't control outputs here
+void Always() { //always runs if void loop is running, don't control outputs here
 
 }
 
@@ -34,13 +51,17 @@ void WifiDataToParse() {
   wifiArrayCounter = 0;
   enabled = recvBl();
   //add data to read here:
-
+  drive = recvFl();
+  turn = recvFl();
+  recvFl();  //throw away strafing
+  intake = recvFl();
+  arm = recvFl();
 }
 int WifiDataToSend() {
   wifiArrayCounter = 0;
   sendFl(batVoltAvg);
   //add data to send here:
-  
+
   return wifiArrayCounter;
 }
 
