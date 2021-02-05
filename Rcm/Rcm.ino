d28471da6c63333b72796be8008b229e78be351f
pickerell <pickerell@marisths.net>
2021-02-05 13:45:06 -0800

    Update Rcm.ino

--- Rcm/Rcm.ino
+++ Rcm/Rcm.ino
@@ -1,28 +1,44 @@
 #include "rcmutil.h"
 #include "wifi.h"
-const char *routerName = "networkName";
-const char *routerPass = "networkPass";
+const char *routerName = "Garage2.4";
+const char *routerPass = "TamiBeach";
 const char *APPass = "RCMpassword";
-int port = 25210;
+int port = 25216;
 const boolean connectToNetwork = true; //true=try to connect to router  false=go straight to hotspot mode
 const boolean wifiRestartNotHotspot = true; //when connection issue, true=retry connection to router  false=fall back to hotspot
 const int SIGNAL_LOSS_TIMEOUT = 1000; //disable if no signal after this many milliseconds
-//////////////////////////// add variables here
+float throttle = 0;
+float yaw = 0;
+float lift = 0;
+float finger = 0;
+float lifter = 0;
 
 void Enabled() { //code to run while enabled
-
+  tankMot(portA, portD, yaw, throttle);
+  setMot(portB, lift);
+////////////////////////
+  setSer(port5,lifter,500,2000);
+  setSer(port4,finger,500,2000);
 }
 
 void Enable() { //turn on outputs
-
+  enableMot(portA);
+  enableMot(portB);
+  enableMot(portD);
+  enableSer(port5);
+  enableSer(port4);
 }
 
 void Disable() { //shut off all outputs
-
+  disableMot(portA);
+  disableMot(portB);
+  disableMot(portD);
+  disableSer(port5);
+  disableSer(port4);
 }
 
 void PowerOn() { //runs once on robot startup, set pinmodes
-
+  setMotorCalibration(2.2, .05);
 }
 
 void Always(){ //always runs if void loop is running, don't control outputs here
@@ -34,8 +50,13 @@ void WifiDataToParse() {
   wifiArrayCounter = 0;
   enabled = recvBl();
   //add data to read here:
-
+throttle = recvFl();
+yaw = recvFl();
+lift = recvFl();
+finger = recvFl();
+lifter = recvFl();
 }
+
 int WifiDataToSend() {
   wifiArrayCounter = 0;
   sendFl(batVoltAvg);
